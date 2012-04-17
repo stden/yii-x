c41d1acd459c75e6aaea0e5e59d1fd0ef202e8e7
Denis Stepulenok <super.denis@gmail.com>
2012-04-17 16:52:51 +0400

    Генератор паролей + HTML вывод

---
+++ MyHTML.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Функции вёрстки, удобные в этом проекте
+ */
+class MyHTML
+{
+    /**
+     * Вставка картинки, только если она существует
+     * @static
+     * @param $path
+     */
+    public static function image($path, $id = null)
+    {
+        $pathOnServer = self::webRoot() . '/' . $path;
+        if (is_null($id))
+            $id = uniqid();
+
+        if (file_exists($pathOnServer)) {
+            echo CHtml::image(self::baseUrl() . '/' . $path, $path,
+                array("class" => "clickme", "title" => 'Фото',
+                    'id' => $id
+                    // "width"=>"200", "height"=>"300"
+                )
+            );
+        } else {
+            echo "Файла $path на сервере нет!";
+        }
+    }
+
+    /**
+     * Базовый URL сайта
+     * @static
+     * @return string
+     */
+    public static function baseUrl()
+    {
+        return Yii::app()->request->baseUrl;
+    }
+
+    /**
+     * Каталог на сервере с корнем сайта
+     * @static
+     * @return mixed
+     */
+    public static function webRoot()
+    {
+        return Yii::getPathOfAlias('webroot');
+    }
+}
---
+++ PasswordGenerator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Генератор паролей заданной сложности
+ */
+class PasswordGenerator
+{
+    /**
+     * Функция генерации
+     * @param int $length
+     * @param int $strength
+     * @return string
+     */
+    static public function generate($length = 6, $strength = 15)
+    {
+        $vowels = 'aeuy';
+        $consonants = 'bdghjmnpqrstvz';
+        if ($strength & 1) {
+            $consonants .= 'BDGHJLMNPQRSTVWXZ';
+        }
+        if ($strength & 2) {
+            $vowels .= "AEUY";
+        }
+        if ($strength & 4) {
+            $consonants .= '23456789';
+        }
+        if ($strength & 8) {
+            $consonants .= '@#$%';
+        }
+
+        $password = '';
+        $alt = time() % 2;
+        for ($i = 0; $i < $length; $i++) {
+            if ($alt == 1) {
+                $password .= $consonants[(rand() % strlen($consonants))];
+                $alt = 0;
+            } else {
+                $password .= $vowels[(rand() % strlen($vowels))];
+                $alt = 1;
+            }
+        }
+        return $password;
+    }
+}
